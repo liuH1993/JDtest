cbeff5b2e11d80a5a3c632eb7fcbef4de78b226f
刘小手 <2464926849@qq.com>
2019-07-12 15:17:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(".rm").click(function () {
+        $(".header").remove();
+    })
+});
+
+// $(function () {
+//     $("#nav-items").find("a").hover(function () {
+//        $(this).css("color","red");
+//     })
+// });
+// $(function () {
+//     $("#nav-items").find("a").mouseout(function () {
+//         $(this).css("color","#333333")
+//     })
+// })
